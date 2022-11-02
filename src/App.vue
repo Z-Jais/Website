eb780420dc5e2baf6504e81809549bf42cec0789
Ziedelth <watteaupierre@gmail.com>
2022-11-02 10:39:40 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <NavBar />
+  </header>
+
+  <ThreadComponent>
+    <div class="thread-container">
+      <img src="./assets/icon.webp" alt="">
+    </div>
+    <div class="thread-container">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac vestibulum magna, non tristique dui. Nulla quis nulla quis mi blandit pretium. Quisque ac turpis sit amet mauris congue elementum sit amet id sapien. Suspendisse a nibh ultrices, blandit nulla id, bibendum eros. In commodo, ante in pulvinar posuere, ligula mauris tincidunt leo, sed posuere metus mi non tortor. Suspendisse id lorem quam. Donec scelerisque consectetur erat at eleifend. Integer sit amet ornare nunc. Sed ipsum nunc, interdum eget auctor pharetra, interdum non nunc. Morbi tristique mollis nunc. Integer suscipit sem et justo lacinia, ut maximus lorem fermentum. Sed consectetur facilisis dolor in rutrum. Integer condimentum sapien nec lacus rutrum scelerisque at quis ante.
+      Sed bibendum bibendum iaculis. Proin nunc felis, rhoncus vel justo eget, aliquam luctus neque. Mauris scelerisque, quam non ullamcorper scelerisque, neque felis pulvinar sapien, et suscipit erat ligula in nunc. Sed sit amet tempor nibh. Sed et cursus tortor. Donec vitae volutpat nisi, nec viverra mauris. Morbi metus diam, varius vitae vehicula eget, mollis ac nulla. In ac urna vitae lectus sodales condimentum in a nibh. Proin convallis in tellus et vestibulum. Fusce dui nisl, rutrum faucibus leo nec, dapibus aliquam nulla. Morbi ac pulvinar turpis, vel pellentesque diam. Suspendisse in turpis eu arcu tempor laoreet. Curabitur eu turpis quam. Curabitur nulla lacus, posuere vitae massa quis, aliquam tincidunt felis. Ut auctor eget elit nec tincidunt.
+      Maecenas nec arcu in orci tincidunt pharetra aliquet mollis est. Nullam sodales enim eget lobortis posuere. Pellentesque semper quam malesuada, convallis neque in, scelerisque ligula. Sed posuere molestie nunc. Aliquam dapibus dui massa, non luctus libero eleifend eget. Nunc tristique lectus arcu, a ullamcorper tortor placerat sit amet. Ut eleifend scelerisque lectus sed dictum. Suspendisse potenti. Suspendisse pellentesque dictum neque, non consequat risus posuere id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut iaculis tortor. Nulla facilisi. Sed velit urna, blandit hendrerit massa at, consequat dictum odio. Vivamus tellus libero, ornare eget dolor nec, rutrum elementum neque.
+    </div>
+  </ThreadComponent>
+
+  <hr>
+
+  <ThreadComponent>
+    <div class="thread-container">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac vestibulum magna, non tristique dui. Nulla quis nulla quis mi blandit pretium. Quisque ac turpis sit amet mauris congue elementum sit amet id sapien. Suspendisse a nibh ultrices, blandit nulla id, bibendum eros. In commodo, ante in pulvinar posuere, ligula mauris tincidunt leo, sed posuere metus mi non tortor. Suspendisse id lorem quam. Donec scelerisque consectetur erat at eleifend. Integer sit amet ornare nunc. Sed ipsum nunc, interdum eget auctor pharetra, interdum non nunc. Morbi tristique mollis nunc. Integer suscipit sem et justo lacinia, ut maximus lorem fermentum. Sed consectetur facilisis dolor in rutrum. Integer condimentum sapien nec lacus rutrum scelerisque at quis ante.
+      Sed bibendum bibendum iaculis. Proin nunc felis, rhoncus vel justo eget, aliquam luctus neque. Mauris scelerisque, quam non ullamcorper scelerisque, neque felis pulvinar sapien, et suscipit erat ligula in nunc. Sed sit amet tempor nibh. Sed et cursus tortor. Donec vitae volutpat nisi, nec viverra mauris. Morbi metus diam, varius vitae vehicula eget, mollis ac nulla. In ac urna vitae lectus sodales condimentum in a nibh. Proin convallis in tellus et vestibulum. Fusce dui nisl, rutrum faucibus leo nec, dapibus aliquam nulla. Morbi ac pulvinar turpis, vel pellentesque diam. Suspendisse in turpis eu arcu tempor laoreet. Curabitur eu turpis quam. Curabitur nulla lacus, posuere vitae massa quis, aliquam tincidunt felis. Ut auctor eget elit nec tincidunt.
+      Maecenas nec arcu in orci tincidunt pharetra aliquet mollis est. Nullam sodales enim eget lobortis posuere. Pellentesque semper quam malesuada, convallis neque in, scelerisque ligula. Sed posuere molestie nunc. Aliquam dapibus dui massa, non luctus libero eleifend eget. Nunc tristique lectus arcu, a ullamcorper tortor placerat sit amet. Ut eleifend scelerisque lectus sed dictum. Suspendisse potenti. Suspendisse pellentesque dictum neque, non consequat risus posuere id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut iaculis tortor. Nulla facilisi. Sed velit urna, blandit hendrerit massa at, consequat dictum odio. Vivamus tellus libero, ornare eget dolor nec, rutrum elementum neque.
+    </div>
+    <div class="thread-container">
+      <img src="./assets/icon.webp" alt="">
+    </div>
+  </ThreadComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "@/components/NavBar";
+import ThreadComponent from "@/components/ThreadComponent";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {ThreadComponent, NavBar},
 }
 </script>
 
 <style>
-#app {
+:root {
+  --logo-size: 32px;
+  --line-height: 1.5;
+
+  --light: #faf6f7;
+  --dark: #313237;
+  --variant: #a38e97;
+}
+
+* {
+  color: var(--dark);
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+hr {
+  border: 0;
+  border-top: 1px solid var(--variant);
+  margin: 0 auto;
+  width: 95%;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="navbar">
+    <img alt="Vue logo" src="../assets/icon.webp" class="navbar-icon">
+    <ul>
+      <li><a href="#" class="active" @click="onLinkClick($event)">Accueil</a></li>
+      <li><a href="#" @click="onLinkClick($event)">Jaïs</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    onLinkClick: function (event) {
+      // Remove the active class from all links
+      const links = document.querySelectorAll('.navbar ul li a');
+
+      links.forEach((link) => {
+        link.classList.remove('active');
+      });
+
+      // Add the active class to the clicked link
+      event.target.classList.add('active');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: var(--logo-size);
+  background-color: var(--light);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px 10px;
+  display: flex;
+}
+
+.navbar-icon {
+  width: var(--logo-size);
+  height: var(--logo-size);
+  object-fit: cover;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+.navbar ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-grow: 1;
+}
+
+.navbar ul li {
+  margin: 0 10px;
+}
+
+.navbar ul li a {
+  color: var(--dark);
+  text-decoration: none;
+  font-size: 18px;
+}
+
+.navbar ul li a.active {
+  color: var(--dark);
+  border-bottom: 2px solid var(--dark);
+  padding-bottom: 8px;
+}
+
+.navbar ul li a:hover {
+  color: var(--dark);
+  border-bottom: 2px solid var(--dark);
+  padding-bottom: 8px;
+  transition: all 0.2s ease-in-out;
+}
+</style>
