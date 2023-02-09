ade365528d8d39b78e9f0eb1ee65319b053a1734
Ziedelth <watteaupierre@gmail.com>
2023-02-09 08:54:46 +0100

    Remove obfuscator due to security

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const JavaScriptObfuscator = require('webpack-obfuscator')
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new JavaScriptObfuscator({
-        rotateUnicodeArray: true,
-      }, ['excluded_bundle_name.js']),
-    ],
-  }
+  transpileDependencies: true
 })
