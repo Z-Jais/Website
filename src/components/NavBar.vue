e139a4d85fc645b7a18557a7fdb0577f291d88c8
Ziedelth <watteaupierre@gmail.com>
2023-04-11 22:11:27 +0200

    Change img and optimize

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,69 +1,68 @@
 <template>
-  <div class="navbar">
-    <img alt="Vue logo" class="navbar-icon" height="32" src="img/icon.webp" width="32">
-    <ul>
-      <li v-for="link in links" :key="link.name">
-        <router-link :to="link.to" exact-active-class="active">{{ link.name }}</router-link>
-      </li>
-    </ul>
-  </div>
+    <div class="navbar">
+        <img alt="Vue logo" class="navbar-icon" height="32" src="../../public/img/icon.webp" width="32">
+        <ul>
+            <li v-for="link in links" :key="link.name">
+                <router-link :to="link.to" exact-active-class="active">{{ link.name }}</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
-  data() {
-    return {
-      links: [
-        {name: 'Accueil', to: '/'},
-        {name: 'Épisodes', to: '/episodes'},
-        {name: 'Animés', to: '/animes'},
-      ]
+    name: 'NavBar',
+    data() {
+        return {
+            links: [
+                {name: 'Accueil', to: '/'},
+                {name: 'Épisodes', to: '/episodes'},
+            ]
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss" scoped>
 .navbar {
-  height: var(--logo-size);
-  background-color: var(--light);
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  padding: 10px 10px;
-  display: flex;
-
-  .navbar-icon {
-    width: var(--logo-size);
     height: var(--logo-size);
-    object-fit: cover;
-    border-radius: 5px;
-    margin-right: 10px;
-  }
-
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
+    background-color: var(--light);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 10px 10px;
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    flex-grow: 1;
 
-    li {
-      margin: 0 10px;
-
-      a {
-        color: var(--dark);
-        text-decoration: none;
-        font-size: 18px;
-
-        &.active {
-          color: var(--dark);
-          border-bottom: 2px solid var(--dark);
-          padding-bottom: 8px;
-        }
-      }
+    .navbar-icon {
+        width: var(--logo-size);
+        height: var(--logo-size);
+        object-fit: cover;
+        border-radius: 5px;
+        margin-right: 10px;
     }
-  }
+
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        flex-grow: 1;
+
+        li {
+            margin: 0 10px;
+
+            a {
+                color: var(--dark);
+                text-decoration: none;
+                font-size: 18px;
+
+                &.active {
+                    color: var(--dark);
+                    border-bottom: 2px solid var(--dark);
+                    padding-bottom: 8px;
+                }
+            }
+        }
+    }
 }
 </style>
--- src/views/EpisodesView.vue
+++ src/views/EpisodesView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div v-if="isLoading" class="center">
-    <SpinnerComponent/>
-  </div>
+    <div v-if="isLoading" class="center">
+        <SpinnerComponent/>
+    </div>
 
-  <div v-else class="list">
-    <EpisodeComponent v-for="episode in episodes" :key="episode.uuid" :episode="episode" class="item"/>
-  </div>
+    <div v-else class="list">
+        <EpisodeComponent v-for="episode in episodes" :key="episode.uuid" :episode="episode" class="item"/>
+    </div>
 </template>
 
 <script>
@@ -14,60 +14,60 @@ import SpinnerComponent from "@/components/SpinnerComponent.vue";
 import Config from "@/config";
 
 export default {
-  name: "EpisodesView",
-  components: {SpinnerComponent, EpisodeComponent},
-  data() {
-    return {
-      isLoading: false,
-      limit: 12,
-      page: 1,
-      episodes: [],
-    }
-  },
-  methods: {
-    async getEpisodes(indicator = true) {
-      if (this.isLoading) {
-        return;
-      }
-
-
-      if (indicator) {
-        this.isLoading = true;
-      }
-
-      const response = await fetch(`${Config.URL}episodes/country/fr/page/${this.page}/limit/${this.limit}`);
-
-      if (!response.ok) {
-        if (indicator) {
-          this.isLoading = false;
+    name: "EpisodesView",
+    components: {SpinnerComponent, EpisodeComponent},
+    data() {
+        return {
+            isLoading: false,
+            limit: 12,
+            page: 1,
+            episodes: [],
         }
-
-        return;
-      }
-
-      const data = await response.json();
-      this.episodes.push(...data);
-
-      if (indicator) {
-        this.isLoading = false;
-      }
     },
-  },
-  mounted() {
-    this.getEpisodes();
+    methods: {
+        async getEpisodes(indicator = true) {
+            if (this.isLoading) {
+                return;
+            }
 
-    const handleScroll = () => {
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      const bottomOfWindow = Math.abs(scrollTop + window.innerHeight - document.documentElement.offsetHeight) <= 5;
 
-      if (bottomOfWindow) {
-        this.page++;
-        this.getEpisodes(false);
-      }
-    };
+            if (indicator) {
+                this.isLoading = true;
+            }
 
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('touchmove', handleScroll);
-  },
+            const response = await fetch(`${Config.URL}episodes/country/fr/page/${this.page}/limit/${this.limit}`);
+
+            if (!response.ok) {
+                if (indicator) {
+                    this.isLoading = false;
+                }
+
+                return;
+            }
+
+            const data = await response.json();
+            this.episodes.push(...data);
+
+            if (indicator) {
+                this.isLoading = false;
+            }
+        },
+    },
+    mounted() {
+        this.getEpisodes();
+
+        const handleScroll = () => {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            const bottomOfWindow = Math.abs(scrollTop + window.innerHeight - document.documentElement.offsetHeight) <= 5;
+
+            if (bottomOfWindow) {
+                this.page++;
+                this.getEpisodes(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('touchmove', handleScroll);
+    },
 }
 </script>
