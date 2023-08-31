f734ee01b9d7cfc0f83da5dd97979e2b39b20617
Ziedelth <watteaupierre@gmail.com>
2023-08-31 19:16:24 +0200

    Add canonical URL updating method in App.vue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    // URL: 'http://192.168.1.97:8080/',
+    HOST_NAME: 'https://jais.ziedelth.fr',
+    // URL: 'http://192.168.1.52:8080/',
     URL: 'https://beta-api.ziedelth.fr/',
 }
